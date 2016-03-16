eaec151a6c3ecc3aa747e0f9d0dacc5eec2c0d1e
Patricio Calderon <ourobor@gmail.com>
2016-03-16 19:18:34 -0400

    adds a listOfArtists function to show artists on the browser

--- art_collecting.js
+++ art_collecting.js
@@ -1,11 +1,28 @@
 
-Artists = new Mongo.Collection('artists')
+Artists = new Mongo.Collection('artists');
+
+var artist = [
+  {
+    name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"
+  }
+];
 
 if (Meteor.isClient) {
 
 Template.creators.helpers({
   collecting: function() {
     return ("I love this piece!");
+  },
+  listOfArtists: function() {
+    return Artists.find();
   }
 });
 
