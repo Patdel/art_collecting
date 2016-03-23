1995545ddc72684d576a20faea4ca2888ae85abd
Patricio Calderon <ourobor@gmail.com>
2016-03-22 22:14:11 -0400

    added a startup function

--- art_collecting.js
+++ art_collecting.js
@@ -18,9 +18,15 @@ var artist = [
 if (Meteor.isClient) {
 
 Template.creators.helpers({
-
   listOfArtists: function() {
     return Artists.find();
+  },
+});
+
+Template.creators.events({
+  'click .thumbnail': function() {
+    var artistId = this._id;
+    console.log(artistId);
   }
 });
 
@@ -30,5 +36,76 @@ Template.creators.helpers({
 }
 
 if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Artists.find().count() === 0) {
+      Artists.insert(
+        {name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"},
 
-}
+      {name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"},
+
+    {name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"},
+
+    {name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"},
+
+    {name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"},
+
+    {name: "Isa Genzken",
+    mediums: ["painting", "installation"] ,
+    location: "Berlin, Germany",
+    work: [{title: "Angel",
+           price: 30000,
+           year: "2004",
+           medium: "mixed media"}],
+    seller: "David Zwirner",
+    picture: "angelisagenzken.jpg"},
+
+    ); // insert function
+    } //if statement
+  }); //startup function
+} // if Server
+
+
+
+
+
+
